aa134c58473b65e8af3b0a8b169922f0d2fa2335
batmax33 <59963537+batmax33@users.noreply.github.com>
2021-01-21 16:32:03 +0100

    Update CapteurDeTension.c

--- CapteurDeTension.c
+++ CapteurDeTension.c
@@ -1,6 +1,6 @@
-int VoltageArduino; //Tension de sortie captée par la carte arduino --> nombre entre 0 et 1023
+float VoltageArduino; //Tension de sortie captée par la carte arduino --> nombre entre 0 et 1023
 int Vcc=5; //tension d'alimentation de l'arduino (ATTENTION : 5 * Vccmax >= VBatterie)
-int Vbatterie; // tension réelle de la batterie
+float Vbatterie; // tension réelle de la batterie
 int R1= 7.5; //valeur de la résistance 1
 int R2= 30;
 int factor= (R1+R2)/R1; //pont diviseur de tension qui donne Vbatterie=factor*VoltageArduino
@@ -12,9 +12,8 @@ void setup ()
 void loop ()
 {
   VoltageArduino= analogRead (0); //lecture de la valeur de la tension de sortie captée par le port 0 de la carte Arduino--> nombre entre 0 et 1023
-  VoltageArduino= (VoltageArduino/1023)*Vcc; //conversion de la valeur en tension comprise entre 0 et 5
-  Vbatterie=factor*VoltageArduino; // pont diviseur de tension
-    Serial.println ("Voltage: ");
+  Vbatterie=factor*Vcc*VoltageArduino/1023; // pont diviseur de tension
+  Serial.println ("Voltage: ");
   Serial.println (Vbatterie); // affichage
   delay (1000);
 }
